c40a2e80e00da127114d5e8f072eafb009f4c484
Guillaume ANTON <anton.guillaume0@gmail.com>
2019-04-04 09:15:13 +0200

    first commit

---
+++ EnigmeDeLaPorte/Motor.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace EnigmeDeLaPorte
+{
+    public class Jeu
+    {
+        static Random rnd = new Random();
+        private Porte[] portes;
+        private int gain;
+        private int gainDuTour;
+        private int numPorte;
+
+        public Jeu()
+        {
+            portes = new Porte[3];
+            gain = 0;
+            gainDuTour = 0;
+        }
+        
+        //etape -1 initialisation
+        public void init()
+        {
+            portes[0] = new Porte();
+            portes[1] = new Porte();
+            portes[2] = new Porte();
+            int numGagnante = rnd.Next(0, 3);
+            //setGagnate
+            portes[numGagnante].setGagnante();
+            gainDuTour = 0;
+        }
+        //choix de la porte de base
+        public void premierChoixDePorteHumain()
+        {
+            do
+            {
+                Console.WriteLine("Choisir une porte (1, 2, 3)");
+                string str = Console.ReadLine();
+                numPorte = int.Parse(str);
+            } while (numPorte != 1 && numPorte != 2 && numPorte != 3);
+            numPorte--;
+            portes[numPorte].setChoix(true);
+        }
+        public void premierChoixDePorteBot()
+        {
+            numPorte = rnd.Next(0, 3);
+            portes[numPorte].setChoix(true);
+            Console.WriteLine("Vous avez choisi la porte " + (numPorte + 1));
+        }
+        //recherche d'une porte perdante
+        public void afficherPorteVide()
+        {
+            for(int i=0; i< 3; i++)
+            {
+                if (portes[i].getChoix() == false && portes[i].getGagante() == false)
+                {
+                    Console.WriteLine("La porte " + (i+1) + " est vide");
+                    portes[i].setOpen();
+                    i = 4; //comme ca on force a quitter le for
+                }
+            }
+        }
+        public void secondChoixDePorteHumain()
+        {
+            do
+            {
+                Console.WriteLine("Choisir une porte (1, 2, 3)");
+                string str = Console.ReadLine();
+                numPorte = int.Parse(str);
+            } while (numPorte != 1 && numPorte != 2 && numPorte != 3);
+            numPorte--;
+            portes[numPorte].setChoix(true);
+        }
+        public void secondChoixDePorteBot()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                if(portes[i].getOpenned() == false && portes[i].getChoix() == false)
+                {
+                    numPorte = i;
+                }
+                else
+                {
+                    portes[i].setChoix(false);
+                }
+            }
+            portes[numPorte].setChoix(true);
+        }
+        public void resultat()
+        {
+            gainDuTour = portes[numPorte].getMontant();
+            Console.WriteLine("Vous avez gagné " + gainDuTour + "€");
+            gain += gainDuTour;
+        }
+
+
+        public int getGain() { return gain; }
+
+    }
+
+    public class Porte
+    {
+        private Boolean gagnante;
+        private int gain;
+        private Boolean choosen;
+        private Boolean openned;
+        //builder
+        public Porte()
+        {
+            gagnante = false;
+            gain = 0;
+            choosen = false;
+            openned = false;
+        }
+        
+        //getters
+        public Boolean getGagante() { return gagnante; }
+        public void setGagnante() { gagnante = true; gain = 1; }
+        public void setPerdante() { gagnante = false; gain = 0; }
+        public int getMontant() { return gain; }
+        public void setChoix(Boolean choix) { choosen = choix; }
+        public Boolean getChoix() { return choosen; }
+        public void setOpen() { openned = true; }
+        public Boolean getOpenned() { return openned; }
+    }
+}
---
+++ EnignmeConsolePourHumain/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using EnigmeDeLaPorte;
+
+namespace EnignmeConsolePourHumain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Jeu jeu = new Jeu();
+            jeu.init();
+            jeu.premierChoixDePorteHumain();
+            jeu.afficherPorteVide();
+            jeu.secondChoixDePorteHumain();
+            jeu.resultat();
+            Console.ReadLine();
+            */
+
+            
+            Jeu jeu = new Jeu();
+            int nbIteration = 100000;
+            for (int i=0; i < nbIteration; i++)
+            {
+                
+                jeu.init();
+                jeu.premierChoixDePorteBot();
+                jeu.afficherPorteVide();
+                jeu.secondChoixDePorteBot();
+                jeu.resultat();
+            }
+
+            Console.WriteLine(jeu.getGain());
+            //Console.WriteLine("Proba de gain : " + (jeu.getGain()/nbIteration));
+            Console.ReadLine();
+            
+        }
+    }
+}
